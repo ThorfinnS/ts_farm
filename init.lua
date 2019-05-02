bef4383db71d1110b747e05e2fb05e12103b1983
ThorfinnS <steveatoni@gmail.com>
2019-05-01 22:19:08 -0500

    added mapgen

--- init.lua
+++ init.lua
@@ -69,151 +69,206 @@ local function define_crops(name, desc, cropgroups, seedgroups, growgroups, matu
 	local seedpng="farming_"..name.."_seed"..".png"
 	local stagename="farming_"..name.."_"
 
-
-	-- concatenate groups if any passed in
-	local seed_grp = {seed = 1, snappy = 3, attached_node = 1}
-	for _, v in pairs(seedgroups) do
-		table.insert(seed_grp, v)
-	end
-
-	local grow_grp = {snappy = 3, flammable = 2, plant = 1, attached_node = 1,
-		not_in_creative_inventory = 1, growing = 1}
-	for _, v in pairs(growgroups) do
-		table.insert(grow_grp, v)
-	end
-
-	local crop_grp = {food_barley = 1, flammable = 2}
-	for _, v in pairs(cropgroups) do
-		table.insert(crop_grp, v)
-	end
-
-
-	-- define crop seed
-	minetest.register_node(seedname, {
-		description = desc.." Seed",
-		tiles = {seedpng},
-		inventory_image = seedpng,
-		wield_image = seedpng,
-		drawtype = "signlike",
-		groups = seed_grp,
-		paramtype = "light",
-		paramtype2 = "wallmounted",
-		walkable = false,
-		sunlight_propagates = true,
-		selection_box = farming.select,
-		on_place = function(itemstack, placer, pointed_thing)
-			return farming.place_seed(itemstack, placer, pointed_thing, cropname.."_1")
-		end,
-	})
-
-	-- define harvested crop
-	minetest.register_craftitem(cropname, {
-		description = desc,
-		inventory_image = name..".png",
-		groups = crop_grp,
-	})
-
-
-	-- define crop growth
-	local crop_def = {
-		drawtype = "plantlike",
-		paramtype = "light",
-		paramtype2 = "meshoptions",
-		place_param2 = 3,
-		sunlight_propagates = true,
-		walkable = false,
-		buildable_to = true,
-		drop = {},
-		selection_box = farming.select,
-		groups = grow_grp,
-		sounds = default.node_sound_leaves_defaults()
-	}
-
-	local u={}
-	-- local v={}
-	local w={}
-	local s={}
-	-- local t={}
-	-- local t1=9
-	for n=1,fully_grown do
-		u={}
-		crop_def.tiles = {stagename..tostring(n)..".png"} 
-		if n>= mature then -- define each stage
-			s=dropper[n-mature+1] -- stage n harvest 
-			crop_def.drop={max_items=s[1]}
-			for m=1,#s-1 do
-				local t=s[m+1] -- 
-				if t[1]=='c' then
-					t[1]=cropname
-				elseif t[1]=='s' then
-					t[1]=seedname
-				end
-				u[#u+1]={items={t[1].." "..tostring(t[2])}, rarity = t[3]}
-			end
-			crop_def.drop={items=u,max_items=s[1]}
-			if n==fully_grown then crop_def.groups.growing = 0 end
-		end
-		minetest.register_node(cropname.."_"..tostring(n), table.copy(crop_def))
-	end
-
-
-		-- add to registered_plants
-		farming.registered_plants[cropname] = {
-			crop = cropname,
-			seed = seedname,
-			minlight = lmin, 
-			maxlight = lmax, 
-			steps = fully_grown
-	}
-
-	-- Fuel
-	minetest.register_craft({
-		type = "fuel",
-		recipe = cropname,
-		burntime = 1,
-	})
-
-	-- flour
-	-- minetest.register_craft({
-		-- type = "shapeless",
-		-- output = "farming:flour",
-		-- recipe = {
-			-- "farming:barley", "farming:barley", "farming:barley",
-			-- "farming:barley", "farming:mortar_pestle"
-		-- },
-		-- replacements = {{"group:food_mortar_pestle", "farming:mortar_pestle"}},
-	-- })
-
-	-- marram grass to thatch
-	minetest.register_craftitem(cropname, {
-		description = "Thatch",
-		inventory_image = croppng,
-		groups = {flammable = 2},
-	})
-
-	minetest.register_craft({
-		type = "shapeless",
-		output = "farming:straw 3",
-		recipe = {
-			cropname
-		}
-	})
-
-	minetest.override_item("default:marram_grass_3", {
-		drop = {
-			max_items = 5, --3,
-			items = {
-				{items = {seedname}},
-				{items = {"default:marram_grass_1"}},
-				{items = {seedname}, rarity = 5},
-				{items = {seedname}, rarity = 12},
-				{items = {cropname}},
-			}
-		},
-	})
-
-
+ local function multiplier()
+	return (math.random()+math.random()+math.random()+math.random())/2
 end
+
+
+-- concatenate groups if any passed in
+local seed_grp = {seed = 1, snappy = 3, attached_node = 1}
+for _, v in pairs(seedgroups) do
+	table.insert(seed_grp, v)
+end
+
+local grow_grp = {snappy = 3, flammable = 2, plant = 1, attached_node = 1,
+	not_in_creative_inventory = 1, growing = 1}
+for _, v in pairs(growgroups) do
+	table.insert(grow_grp, v)
+end
+
+local crop_grp = {food_barley = 1, flammable = 2}
+for _, v in pairs(cropgroups) do
+	table.insert(crop_grp, v)
+end
+
+
+-- define crop seed
+minetest.register_node(seedname, {
+	description = desc.." Seed",
+	tiles = {seedpng},
+	inventory_image = seedpng,
+	wield_image = seedpng,
+	drawtype = "signlike",
+	groups = seed_grp,
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	walkable = false,
+	sunlight_propagates = true,
+	selection_box = farming.select,
+	on_place = function(itemstack, placer, pointed_thing)
+		return farming.place_seed(itemstack, placer, pointed_thing, cropname.."_1")
+	end,
+})
+
+-- define harvested crop
+minetest.register_craftitem(cropname, {
+	description = desc,
+	inventory_image = name..".png",
+	groups = crop_grp,
+})
+
+
+-- define crop growth stages
+local crop_def = {
+	drawtype = "plantlike",
+	paramtype = "light",
+	paramtype2 = "meshoptions",
+	place_param2 = 3,
+	sunlight_propagates = true,
+	walkable = false,
+	buildable_to = true,
+	drop = {},
+	selection_box = farming.select,
+	groups = grow_grp,
+	sounds = default.node_sound_leaves_defaults()
+}
+
+local u={}
+local w={}
+local s={}
+for n=1,fully_grown do
+	u={}
+	crop_def.tiles = {stagename..tostring(n)..".png"} 
+	if n>= mature then -- define each stage
+		s=dropper[n-mature+1] -- stage n harvest 
+		crop_def.drop={max_items=s[1]}
+		for m=1,#s-1 do
+			local t=s[m+1] -- 
+			if t[1]=='c' then
+				t[1]=cropname
+			elseif t[1]=='s' then
+				t[1]=seedname
+			end
+			u[#u+1]={items={t[1].." "..tostring(t[2])}, rarity = t[3]}
+		end
+		crop_def.drop={items=u,max_items=s[1]}
+		if n==fully_grown then crop_def.groups.growing = 0 end
+	end
+	minetest.register_node(cropname.."_"..tostring(n), table.copy(crop_def))
+end
+
+
+	-- add to registered_plants
+	farming.registered_plants[cropname] = {
+		crop = cropname,
+		seed = seedname,
+		minlight = lmin, 
+		maxlight = lmax, 
+		steps = fully_grown
+}
+
+-- Fuel
+minetest.register_craft({
+	type = "fuel",
+	recipe = cropname,
+	burntime = 1,
+})
+
+-- flour
+-- minetest.register_craft({
+	-- type = "shapeless",
+	-- output = "farming:flour",
+	-- recipe = {
+		-- "farming:barley", "farming:barley", "farming:barley",
+		-- "farming:barley", "farming:mortar_pestle"
+	-- },
+	-- replacements = {{"group:food_mortar_pestle", "farming:mortar_pestle"}},
+-- })
+
+-- marram grass to thatch
+minetest.register_craftitem(cropname, {
+	description = "Thatch",
+	inventory_image = croppng,
+	groups = {flammable = 2},
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "farming:straw 3",
+	recipe = {
+		cropname
+	}
+})
+
+minetest.override_item("default:marram_grass_3", {
+	drop = {
+		max_items = 3, 
+		items = {
+			{items = {cropname}},
+			{items = {seedname}},
+			{items = {seedname}, rarity = 5},
+			{items = {seedname}, rarity = 8},
+		}
+	},
+})
+
+
+-- decoration function
+-- local function register_plant(name, min, max, spawnon, spawnby, num, rarety)
+
+-- do not place on mapgen if no value given (or not true)
+-- if not rarety then
+	-- return
+-- end
+if deco_name=='' or deco_name==nil then return end
+if variable_farming_rarity == true then scaler = scaler * multiplier() end
+if spawnon == nil then spawnon = {"default:dirt_with_grass"} end
+-- minetest.log(name.."  Rarity: "..scaler)
+local seeder = math.random(999)
+
+-- Next code block is legacy support for TenPlus1's fixed seeds.	
+-- Kill the whole block if you don't care about static mapgen.
+-- From here:
+if random_crop_seed == false then -- No, I want my TenPlus1's static seeds!
+	if string.find(name, "hemp")  == 1 then 
+		seeder = 420
+	elseif string.find(name, "chili") == 1 then
+		seeder = 760
+	elseif string.find(name, "pepper") == 1 then
+		seeder = 933
+	elseif string.find(name, "pineapple") == 1 then
+		seeder = 917
+	else
+		seeder = 329
+	end
+end
+-- To here.	
+
+
+
+minetest.register_decoration({
+	deco_type = "simple",
+	place_on = spawnon,
+	sidelen = 16,
+	noise_params = {
+		offset = 0,
+		scale = scaler, 
+		spread = {x = 100, y = 100, z = 100},
+		seed = seeder,
+		octaves = 3,
+		persist = 0.6
+	},
+	y_min = min,
+	y_max = max,
+	decoration = deco_name,
+	spawn_by = spawnby,
+	num_spawn_by = num,
+})
+end
+
+
+
+
 
 
 -- wooden_bucket mod
@@ -327,6 +382,6 @@ local function define_crops(
 define_crops("marram","Marram Grass",{},{},{},5,6,12,15,
 	{{9,{'c',1,3},{'s',1,2}},
 	{9,{'c',2,1},{'c',1,3},{'s',1,1},{'s',1,3}}},
-	0, 0, nil, nil, 0, 1, nil,"")
+	0, 0, nil, nil, 0, 1, 329,"")
 
 
