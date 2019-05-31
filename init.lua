832476deffe404d7a8151023f315ea3adf976e4f
ThorfinnS <steveatoni@gmail.com>
2019-05-31 10:21:59 -0500

    begin coding for crop group mill

--- init.lua
+++ init.lua
@@ -3,32 +3,34 @@ local modpath = minetest.get_modpath(modname)
 
 local function define_crops(name, desc, cropgroups, seedgroups, growgroups, mature,
 	fully_grown, lmin, lmax, dropper,
-	hmin, hmax, seed, rarity, dec_name, spawnon, spawnby, num)
+	hmin, hmax, seed, rarity, place_it, dec_name, spawnon, spawnby, num)
 
-local m,s,f,p,u=modname..':','seed','farming_'..name,'.png','_'
-local cropname,seedname,stagename=m..name,m..s..u..name,f..u
-local croppng,seedpng=f..p,f..u..s..p
+local mc,s,p,u=modname..':','seed','.png','_'
+local cropname=mc..name
+local seedname,stagename=mc..s..u..name,name..u
+local croppng,seedpng=name..p,name..u..s..p
+-- Goofy naming convention, I know. Why the nodename is mod:seed_crop instead of
+--		mod:crop_seed, like the rest. Make more coding...?
+--		I'm going to preserve current naming for existing games. Maybe it
+--		serves some purpose. Group all seeds together in some inventory?
+
 
 local function multiplier()
 	return (math.random()+math.random()+math.random()+math.random())/2
 end
 
-
 -- concatenate groups if any passed in
 local seed_grp = {seed = 1, snappy = 3, attached_node = 1}
 for _, v in pairs(seedgroups) do seed_grp[#seed_grp+1]=v
-	-- table.insert(seed_grp, v)
 end
 
 local grow_grp = {snappy = 3, flammable = 2, plant = 1, attached_node = 1,
 	not_in_creative_inventory = 1, growing = 1}
 for _, v in pairs(growgroups) do grow_grp[#grow_grp+1]=v
-	-- table.insert(grow_grp, v)
 end
 
-local crop_grp = {food_barley = 1, flammable = 2}
+local crop_grp = {flammable = 2}
 for _, v in pairs(cropgroups) do crop_grp[#crop_grp+1]=v
-	-- table.insert(crop_grp, v)
 end
 
 
@@ -50,12 +52,23 @@ minetest.register_node(seedname, {
 	end,
 })
 
+
 -- define harvested crop
 minetest.register_craftitem(cropname, {
 	description = desc,
 	inventory_image = croppng,
-	groups = crop_grp,
+	-- groups = crop_grp
 })
+
+-- define products from harvested crop
+minetest.register_craft({
+	type = "shapeless",
+	output = "farming:straw",
+	recipe = {
+		cropname
+	}
+})
+
 
 
 -- define crop growth stages
@@ -75,10 +88,12 @@ local crop_def = {
 
 local u={}
 local v,w,s=u,u,u
+minetest.log('stagename='..stagename)
+
 for n=1,fully_grown do
 	u=v
 	crop_def.tiles = {stagename..tostring(n)..".png"} 
-	if n>= mature then -- define each stage
+	if n>= mature then -- define drops for each stage
 		s=dropper[n-mature+1] -- stage n harvest 
 		crop_def.drop={max_items=s[1]}
 		for m=1,#s-1 do
@@ -89,6 +104,9 @@ for n=1,fully_grown do
 				t[1]=seedname
 			end
 			u[#u+1]={items={t[1].." "..tostring(t[2])}, rarity = t[3]}
+		end
+		if n==mature and dec_name=="" then -- default to place mature if nothing passed
+			dec_name=mc..name.."_"..tostring(n)
 		end
 		crop_def.drop={items=u,max_items=s[1]}
 		if n==fully_grown then crop_def.groups.growing = 0 end
@@ -114,41 +132,48 @@ minetest.register_craft({
 })
 
 -- flour
-if minetest.get_item_group(cropname,'mill')==1 then 
+if minetest.get_item_group(cropname,'mill')==2 then -- generates its own flour
 	minetest.register_craftitem(cropname..'_flour', {
 		description = desc..' Flour',
 		inventory_image = croppng,
-		groups = {flammable = 2, flour=1},
+		groups = {flammable = 2, flour=2},
 	})
 
 	minetest.register_craft({
 		type = "shapeless",
-		output = cropname..'_flour',
+		output = modname..':'..cropname..'_flour',
 		recipe = {
 			cropname, cropname, cropname,
 			cropname, "farming:mortar_pestle"
 		},
 		replacements = {{"group:food_mortar_pestle", "farming:mortar_pestle"}},
 	})
+
+elseif minetest.get_item_group(cropname,'mill')==1 then -- makes generic flour
+	minetest.register_craft({
+		type = "shapeless",
+		output = 'farming:flour',
+		recipe = {
+			cropname, cropname, cropname,
+			cropname, "farming:mortar_pestle"
+		},
+		replacements = {{"group:food_mortar_pestle", "farming:mortar_pestle"}},
+	})
+
 end
 
-
-
-
-
--- Code inspired by TenPlus1's Farming Redo
-if dec_name~='' or dec_name~=nil then
+if place_it then
 	local v=minetest.settings:get_bool("variable_crop_rarity") or true
 	local r=minetest.settings:get_bool("random_crop_seed") or true
 	if v == true then rarity = rarity * multiplier() end
-	if spawnon == nil then spawnon = {"default:dirt_with_grass"} end
-	minetest.log(name.."  Rarity: "..rarity)
+	if spawnon==nil then spawnon = {"default:dirt_with_grass"} end
+	minetest.log(dec_name.."  Rarity: "..rarity)
 	local seeder = math.random(999)
 
 -- Next code block is legacy support for TenPlus1's fixed seeds.	
 -- Kill the whole block if you don't care about static mapgen.
 -- From here:
-	if r == false then -- No, I want my TenPlus1's static seeds!
+	if false then -- I want TenPlus1's static seeds!
 		if string.find(name, "hemp")  == 1 then 
 			seeder = 420
 		elseif string.find(name, "chili") == 1 then
@@ -164,6 +189,9 @@ if dec_name~='' or dec_name~=nil then
 -- To here.	
 
 
+minetest.log(hmin)
+minetest.log(hmax)
+minetest.log(dec_name)
 minetest.register_decoration({
 	deco_type = "simple",
 	place_on = spawnon,
@@ -189,13 +217,6 @@ end
 -- --------------------------------------------BEGIN MAIN CODE ---------------------------
 -- ---------------------------------------------------------------------------------------
 
-
-dofile(modpath..'/newblocks.lua')
-dofile(modpath..'/mod_tweaks.lua')
-dofile(modpath..'/overrides.lua')
-dofile(modpath..'/recipes.lua')
-
-
 --[[
 define_crops(
 field -- type- description
@@ -210,7 +231,7 @@ field -- type- description
 	lmax	i-maximum light for the plant to grow
 	{
 	{max_items,				i- maximum items from growth stage 
-	{drop, qty, rarity}		t- first stage drops
+	{drop, qty, rarity}		t- first stage drops, drop is "c"=crop, "s"=seed, or nodename
 	[,{drop2, qty2, rarity2}
 	[...] ]} 
 	},
@@ -218,7 +239,8 @@ field -- type- description
 	hmin,		i- minimum placement elevation (mapgen) 
 	hmax,		i- maximum placement elevation  (mapgen)
 	seed		i- sigh. some people want repeatable mapgens. just put in some integer
-	rarity		f- .01 is thick, .0001 is very rare. I use around .0005
+	rarity		f- .01 is common, .0001 is very rare. I use around .0005
+	place_it		b- true for place on mapgen, false to ignore
 	dec_name	t- node to place during mapgen
 	spawnon,	t- block mapgen places the crop on
 	spawnby,	t- block mapgen places the crop next to
@@ -230,6 +252,7 @@ field -- type- description
 local grass = "default:dirt_with_grass"
 local dry = "default:dirt_with_dry_grass"
 local jungle = "default:dirt_with_rainforest_litter"
+local sand = "default:sand"
 -- for future crops for under-served biomes?
 -- local forest = "default:dirt_with_coniferous_litter" -- raspberries?
 -- local snow = "default:dirt_with_snow" -- cloudberries?
@@ -243,11 +266,11 @@ define_crops(
 	5,6,12,15, --mature, fully_grown, lmin, lmax
 --drops
 	{
-	{9,{'c',1,3},{'s',1,2}}, -- mature stage, {max items,{drop,qty,rarity}...} 
+	{9,{'c',1,3},{'s',1,1}}, -- mature stage, {max items,{drop,qty,rarity}...} 
 	{9,{'c',2,1},{'c',1,3},{'s',1,1},{'s',1,3}} --next stage, repeat as needed
 	},
 -- mapgen parameters
-	1, 100, 329, .0005,"", --hmin, hmax, seed, rarity, dec_name
-	nil, nil,-1) --spawnon, spawnby, num
+	4, 6, 329, .03,true,"", --hmin, hmax, seed, rarity, place_it, dec_name
+	{sand}, '', -1) --spawnon, spawnby, num
 
 
